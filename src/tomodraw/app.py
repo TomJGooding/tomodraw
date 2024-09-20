56a19df54dd4be2efc09c47f2e7af101e91b00b3
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-09-20 12:32:25 +0100

    feat: add drawing by dragging mouse

--- src/tomodraw/app.py
+++ src/tomodraw/app.py
@@ -15,16 +15,28 @@ class Canvas(Widget):
     """
 
     grid: list[list[str]] = [[" "] * 80 for _ in range(24)]
+    draw: bool = False
 
     def render_line(self, y: int) -> Strip:
         return Strip([Segment(self.grid[y][x]) for x in range(80)])
 
-    def on_mouse_down(self, event: events.MouseDown) -> None:
-        self.draw_cell(event.x, event.y, "x")
-
     def draw_cell(self, x: int, y: int, char: str) -> None:
         self.grid[y][x] = char
         self.refresh()
+
+    def on_mouse_down(self, event: events.MouseDown) -> None:
+        self.draw = True
+        self.draw_cell(event.x, event.y, "x")
+
+    def on_mouse_move(self, event: events.MouseMove) -> None:
+        if self.draw:
+            self.draw_cell(event.x, event.y, "x")
+
+    def on_mouse_up(self, _: events.MouseDown) -> None:
+        self.draw = False
+
+    def on_leave(self, _: events.Leave) -> None:
+        self.draw = False
 
 
 class TomodrawApp(App):
