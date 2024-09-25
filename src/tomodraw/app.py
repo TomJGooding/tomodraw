563125c3a3ab506648bb3db40cb8fbccbdba35b0
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-09-25 19:18:13 +0100

    add basic pencil brush char selection

--- src/tomodraw/app.py
+++ src/tomodraw/app.py
@@ -3,13 +3,14 @@ import enum
 from rich.segment import Segment
 from textual import events, on
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal
+from textual.containers import Container, Horizontal
+from textual.coordinate import Coordinate
+from textual.geometry import Offset
+from textual.screen import ModalScreen
 from textual.strip import Strip
 from textual.widget import Widget
-from textual.widgets import Button, Label, ListView
+from textual.widgets import Button, DataTable, Label, ListView
 from textual.widgets._list_item import ListItem
-
-DEFAULT_PENCIL_CHAR = "x"
 
 
 class Tool(enum.Enum):
@@ -42,7 +43,7 @@ class Canvas(Widget):
         assert isinstance(self.app, TomodrawApp)
         self.app.draw = True
         if self.app.tool == Tool.PENCIL:
-            self.draw_cell(event.x, event.y, self.app.pencil_char)
+            self.draw_cell(event.x, event.y, self.app.pencil_brush_char)
         elif self.app.tool == Tool.ERASER:
             self.erase_cell(event.x, event.y)
 
@@ -51,7 +52,7 @@ class Canvas(Widget):
         if not self.app.draw:
             return
         if self.app.tool == Tool.PENCIL:
-            self.draw_cell(event.x, event.y, self.app.pencil_char)
+            self.draw_cell(event.x, event.y, self.app.pencil_brush_char)
         elif self.app.tool == Tool.ERASER:
             self.erase_cell(event.x, event.y)
 
@@ -98,6 +99,7 @@ class PencilTool(ToolboxItem):
                 border: solid $panel-lighten-3;
             }
             &.-active {
+                background: transparent;
                 border: solid $panel-lighten-3;
             }
         }
@@ -109,17 +111,106 @@ class PencilTool(ToolboxItem):
 
             Button {
                 border: solid $accent-lighten-3;
+                &:hover {
+                    border: solid $accent-lighten-3;
+                }
+                &.-active {
+                    border: solid $accent-lighten-3;
+                }
             }
         }
     }
     """
 
+    brush_char = "x"
+
     def __init__(self) -> None:
         super().__init__(
             Label("Pencil"),
-            Button(DEFAULT_PENCIL_CHAR),
+            Button(self.brush_char),
             tool=Tool.PENCIL,
         )
+
+    @on(Button.Pressed)
+    def on_button_pressed(self) -> None:
+        def update_brush_char(char) -> None:
+            self.brush_char = char
+            self.query_one(Button).label = char
+
+        self.app.push_screen(
+            PencilSelectScreen(
+                self.brush_char,
+                offset_content=self.region.offset + Offset(self.size.width, 0),
+            ),
+            callback=update_brush_char,
+        )
+
+
+class PencilSelectScreen(ModalScreen):
+    DEFAULT_CSS = """
+    PencilSelectScreen {
+        background: transparent;
+
+        Container {
+            width: auto;
+            height: auto;
+            background: $panel-lighten-1;
+        }
+
+        DataTable {
+            width: auto;
+            height: auto;
+        }
+    }
+    """
+
+    BRUSH_CHAR_GRID = [
+        ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+        ["k", "l", "m", "n", "o", "p", "q", "r", "s", "t"],
+        ["u", "v", "w", "x", "y", "z", "A", "B", "C", "D"],
+        ["E", "F", "G", "H", "I", "J", "K", "L", "M", "N"],
+        ["O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"],
+        ["Y", "Z"],
+    ]
+
+    def __init__(self, curr_brush_char: str, offset_content: Offset) -> None:
+        super().__init__()
+        self.selected_brush_char = curr_brush_char
+        self.offset_content = offset_content
+
+    def compose(self) -> ComposeResult:
+        container = Container()
+        container.offset = self.offset_content
+        with container:
+            yield DataTable(show_header=False)
+
+    def on_mount(self) -> None:
+        table = self.query_one(DataTable)
+        table.add_columns(*[""] * len(self.BRUSH_CHAR_GRID[0]))
+        table.add_rows(self.BRUSH_CHAR_GRID)
+        table.cursor_coordinate = self.get_initial_coordinate()
+
+    def get_initial_coordinate(self) -> Coordinate:
+        for row_idx, row in enumerate(self.BRUSH_CHAR_GRID):
+            if self.selected_brush_char in row:
+                return Coordinate(row_idx, row.index(self.selected_brush_char))
+        raise ValueError("Invalid brush character")
+
+    @on(DataTable.CellSelected)
+    def on_data_table_cell_selected(
+        self,
+        event: DataTable.CellSelected,
+    ) -> None:
+        self.selected_brush_char = str(event.value)
+        self.call_after_refresh(self.safe_dismiss)
+
+    def on_click(self, event: events.Click) -> None:
+        clicked, _ = self.get_widget_at(event.screen_x, event.screen_y)
+        if clicked is self:
+            self.safe_dismiss()
+
+    def safe_dismiss(self) -> None:
+        self.dismiss(self.selected_brush_char)
 
 
 class EraserTool(ToolboxItem):
@@ -149,6 +240,10 @@ class TomodrawApp(App):
         align: center middle;
     }
 
+    PencilSelectScreen {
+        align: left top;
+    }
+
     Horizontal {
         width: auto;
         height: auto;
@@ -157,8 +252,6 @@ class TomodrawApp(App):
 
     tool: Tool = Tool.PENCIL
     draw: bool = False
-
-    pencil_char: str = DEFAULT_PENCIL_CHAR
 
     def compose(self) -> ComposeResult:
         with Horizontal():
@@ -169,6 +262,10 @@ class TomodrawApp(App):
     def on_toolbox_tool_selected(self, event: Toolbox.Selected) -> None:
         assert isinstance(event.item, ToolboxItem)
         self.tool = event.item.tool
+
+    @property
+    def pencil_brush_char(self) -> str:
+        return self.query_one(PencilTool).brush_char
 
 
 def run() -> None:
